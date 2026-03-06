c7ffdb6b1f8e9b9cc84a3a0c86959b0a76eba74d
Roganda Tua Tampubolon <rogandatampubolon2007@gmail.com>
2026-03-06 15:23:16 +0700

    Add BidangDuaDimensi class with basic methods

---
+++ Semester2/Latihan_Pertama/BidangDuaDimensi.java
@@ -0,0 +1,19 @@
+class BidangDuaDimensi {
+    private String nama;
+
+    public BidangDuaDimensi(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void info() {
+        System.out.println("Nama: " + nama);
+    }
+
+    public double luas() {
+        return 0; 
+    }
+}
